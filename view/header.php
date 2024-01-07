538ecfbe8f78542f8b2639189389aa31534f08bb
Pau <paust2001@gmail.com>
2024-01-07 19:34:31 +0100

    menus desplegable

--- view/header.php
+++ view/header.php
@@ -15,16 +15,19 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="nav nav-underline navbar-nav mr-manual mb-2 mb-lg-0 padB">
                         <li class="nav-item">
-                            <a class="navlinkP" aria-current="page" href="#">Sobre Natura</a>
+                            <a class="nav-link navlinkP" aria-current="page" href="#">Sobre Natura</a>
                         </li>
                         <li class="nav-item">
-                            <a class="navlinkP" href="#">Compromiso Natura</a>
+                            <a class="nav-link navlinkP" href="#">Compromiso Natura</a>
                         </li>
                     </ul>
-                    <div class="mx-auto">
+                    <div class="mx-auto d-flex justify-content-center align-items-center">
                         <a href="<?=url."?controller=producto"?>">
                             <img class="logo" src="../assets/icons/logo.png" alt="Logo Natura"/>
                         </a>
@@ -37,7 +40,7 @@
                             <input class="buscar" type="text" placeholder="Buscar"/>
                         </li>
                         <li class="nav-item">
-                            <a class="navlinkP" aria-current="page" href="<?=url."?controller=user&action=login"?>">Iniciar sesión</a>
+                            <a class="nav-link navlinkP" aria-current="page" href="<?=url."?controller=user&action=login"?>">Iniciar sesión</a>
                         </li>
                         <li class="nav-item">
                             <a href="<?=url."?controller=producto&action=carrito"?>" class="carrito-link">
