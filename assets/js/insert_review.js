ac7ca75a80b92dd488d9085794a4b0cec116fa4f
Pau <paust2001@gmail.com>
2024-01-22 17:57:56 +0100

    insert de comentario a traves de url

--- assets/js/insert_review.js
+++ assets/js/insert_review.js
@@ -12,45 +12,26 @@ document.addEventListener('DOMContentLoaded', function () {
         const comentario = document.querySelector('input[name="comentario"]').value;
         const rate = document.querySelector('input[name="rate"]:checked').value;
 
-        // Crear un objeto con los datos del formulario
-        const formData = {
-            comentario: comentario,
-            rate: rate,
-            accion: 'insert_reviews'
-        };
-
         // Realizar la solicitud POST a la API
         fetch("http://naturarestaurant.com/index.php/?controller=api&action=api", {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
             },
-            body: JSON.stringify(formData),
+            body: new URLSearchParams({
+                accion: 'insert_reviews',
+                comentario: comentario,
+                rate: rate,
+            }),
         })
         .then(response => {
-            response.json();
+            return response.json();
         })
         .then(data => {
-            mostrarMensaje(data);
+            console.log(data)
         })
         .catch(error => {
-            // Manejar el error si la solicitud falla
             console.error(error);
         });
     });
 });
-
-function mostrarMensaje(mensaje) {
-    // Obtener el elemento div con ID "msg"
-    var msgDiv = document.getElementById('msg');
-
-    // Crear un elemento de párrafo
-    var parrafo = document.createElement('p');
-
-    // Establecer el contenido del párrafo como el mensaje proporcionado
-    parrafo.textContent = mensaje;
-
-    // Agregar el párrafo al div
-    msgDiv.appendChild(parrafo);
-}
-
